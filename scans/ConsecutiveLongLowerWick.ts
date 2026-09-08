125ee227eca7c99b1d7e76bb6feecfbb011c3c64
divyeshramani <diveysh.ramani@gmail.com>
2026-09-07 19:19:55 -0700

    scans updated

--- scans/ConsecutiveLongLowerWick.ts
+++ scans/ConsecutiveLongLowerWick.ts
@@ -7,7 +7,8 @@ input minBody = 0.0;
 
 def body = AbsValue(close - open);
 def lowerWick = Min(open, close) - low;
+def upperWick = high - Max(open, close);
 
-def longLowerWick = lowerWick > wickToBodyMultiple * body and body >= minBody and lowerWick > 0;
+def longLowerWick = lowerWick > wickToBodyMultiple * body and body >= minBody and lowerWick > 0 and lowerWick > upperWick;;
 
 plot scan = longLowerWick and longLowerWick[1];
